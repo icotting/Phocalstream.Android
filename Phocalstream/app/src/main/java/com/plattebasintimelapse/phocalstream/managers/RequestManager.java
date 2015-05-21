cf7b0ff8b774cc43ca5a245ef2c59be9d1b0fd5a
Zach Christensen <zm.christensen@cox.net>
2015-05-21 16:58:59 -0500

    Added upload image method to request manager.

--- Phocalstream/app/src/main/java/com/plattebasintimelapse/phocalstream/managers/RequestManager.java
+++ Phocalstream/app/src/main/java/com/plattebasintimelapse/phocalstream/managers/RequestManager.java
@@ -5,9 +5,13 @@ import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -71,5 +75,77 @@ public class RequestManager {
         }
 
         return null;
+    }
+
+    public static String[] uploadImage(String url_string, String filePath, String token){
+
+        HttpURLConnection conn = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary =  "*****";
+
+        try {
+            // open a URL connection to the Servlet
+            FileInputStream fileInputStream = new FileInputStream(new File(filePath.split(":")[1]) );
+            URL url = new URL(url_string);
+
+            // Open a HTTP connection to the URL
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+
+            // Use a post method.
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+//            conn.setRequestProperty("Authorization", "Bearer " + token);
+
+            // Image
+            DataOutputStream dos = new DataOutputStream( conn.getOutputStream() );
+            dos.writeBytes(lineEnd + twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"image\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+            dos.writeBytes("Image Upload");
+
+            dos.writeBytes(lineEnd + twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"photo\";filename=\"photo.jpg\"" + lineEnd);
+            dos.writeBytes("Content-Type: application/octet-stream"+lineEnd+lineEnd);
+
+            // create a buffer of maximum size
+            int bytesAvailable = fileInputStream.available();
+            int maxBufferSize = 1*1024*1024;
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            byte[] buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            // send multipart form data necesssary after <span id="IL_AD12" class="IL_AD">file data</span>...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // close streams
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
+        }
+        catch (IOException ioe) {
+            Log.e("Phocalstream", "error: " + ioe.getMessage(), ioe);
+        }
+
+        try {
+            return new String[] {String.valueOf( conn.getResponseCode()), "Upload Successful."};
+        }
+        catch (Exception e) {
+            Log.e("Phocalstream", "Exception : " + e.getMessage(), e);
+            return new String[] {"-1", "Error"};
+        }
     }
 }
